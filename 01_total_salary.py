1985c2e82bc826d04150a50b7aae54a36ede7fa5
AnybodyKnows <evgeniiolavrik@gmail.com>
2024-02-03 19:32:16 +0100

    total_salary done

--- 01_total_salary.py
+++ 01_total_salary.py
@@ -1,12 +1,15 @@
-import sys
 
-with open("s.txt","r") as file1:
-    lines = [ l for l in file1.readlines()] 
-    print(lines) 
-    total = 0
-    for i in lines:
-        str= i.split(",")
-        total = total + float(str[1])
-        avg = total / len(lines)
-    print(f"Total={total}  AVG = {avg}")
+
+def total_salary(path):
+    with open(path,"r") as file1:
+        lines = [ l for l in file1.readlines()] 
+        print(lines) 
+        total = 0
+        for i in lines:
+            str= i.split(",")
+            total = total + float(str[1])
+            avg = total / len(lines)
+        return(f"Total={total}  AVG = {avg}")
+
+print(total_salary("s.txt"))
 
