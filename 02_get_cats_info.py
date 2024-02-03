eb39bdb6fe046f443ac5fc04758d1489e04d7d9a
AnybodyKnows <evgeniiolavrik@gmail.com>
2024-02-03 22:19:49 +0100

    02 Done

--- 02_get_cats_info.py
+++ 02_get_cats_info.py
@@ -1,10 +1,18 @@
 
 def get_cats_info(path):
-    with open(path,"r") as file1:
-        lines = [ l for l in file1.readlines()] 
-        print(lines)
-        for i in lines:
-            str= i.split(",")
-            print(str) 
-    
+    try: 
+        with open(path,"r") as file1:
+            cat_inf = []
+            for lines in file1:
+                str = lines.strip()
+                str = str.split(",")
+                keys = ["id", "name", "age"]
+                dict_tmp = dict(zip(keys,str))
+                cat_inf.append(dict_tmp)
+    except:
+        return f"file {path} not found"  
+    return  cat_inf       
+
 print(get_cats_info("cats.txt"))
+
+
