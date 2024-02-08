779dc1013b7b5d9b1a2e8fc6838c4c10aec1a8e6
AnybodyKnows <evgeniiolavrik@gmail.com>
2024-02-08 23:23:47 +0100

    03 begining

--- hw_venv/03_show_dir.py
+++ hw_venv/03_show_dir.py
@@ -1,2 +1,12 @@
-import colorama
-import 
+from colorama import Fore, Back, Style
+import numpy
+from pathlib import Path
+
+directory = Path("hw_venv")
+
+for path in directory.iterdir():
+    if Path.is_dir(path):
+        print(Fore.RED, f"|-{path}")
+    else:
+        print(Fore.GREEN, f"   -{path}")
+
