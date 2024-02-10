1c99f68245a6f2111fd25fbe658afa523f186aa1
AnybodyKnows <evgeniiolavrik@gmail.com>
2024-02-10 11:49:14 +0100

    03 work

--- hw_venv/03_show_dir.py
+++ hw_venv/03_show_dir.py
@@ -2,11 +2,18 @@ from colorama import Fore, Back, Style
 import numpy
 from pathlib import Path
 
-directory = Path("hw_venv")
+# directory = Path("hw_venv")
 
-for path in directory.iterdir():
-    if Path.is_dir(path):
-        print(Fore.RED, f"|-{path}")
-    else:
-        print(Fore.GREEN, f"   -{path}")
+def read_path(folder, space = 0):
+    directory = Path(folder)
+    for path in directory.iterdir():
+        if Path.is_dir(path):
+            print(Fore.RED, f"{" "*space}|-{path}")
+            read_path(path, space = space +1)
 
+        else:
+            print(Fore.GREEN, f"{" "*space}-{path}")
+        
+
+if __name__ == "__main__":
+    read_path("hw_venv\\Scripts")
